53ff93b1569e5e6c689552d84f4e9acb8a724338
densflo <dennis.flores@outlook.ph>
2024-12-24 18:54:12 +0800

    update

--- Checkmk App Instance Updates/Main_updater.ps1
+++ Checkmk App Instance Updates/Main_updater.ps1
@@ -4,14 +4,18 @@
 . "C:\Users\d_flores\OneDrive - TP ICAP\Documents\Code\Checkmk App Instance Updates\Get-cmkeTag.ps1"
 . "C:\Users\d_flores\OneDrive - TP ICAP\Documents\Code\Checkmk App Instance Updates\get-cmklabel.ps1"
 . "C:\Users\d_flores\OneDrive - TP ICAP\Documents\Code\Checkmk App Instance Updates\get-cmklist.ps1"
+Import-Module "C:\Users\d_flores\OneDrive - TP ICAP\Documents\Code\Chekmk-Powershell\CheckMK-PowerShell\CheckMK.psm1" -Force
 
-$cmklist = 'hkgpotlsql02.corp.ad.tullib.com'
+#$cmklist = get-cmklist
+$cmklist = "LDN1WS7001.corp.ad.tullib.com"
+$securepassword = ConvertTo-SecureString "BYGBHSUBPQUIYPSYVVSV" -AsPlainText -Force
+$connection = Get-CMKConnection -Hostname "cmk-emea" -Sitename "London" -Username 'automation' -Secret $securepassword
 
 foreach ($cmk in $cmklist){
     $cmkapp = get-appa2rm -ComputerName $cmk
     foreach ($app in $cmkapp.ApplicationName){
         Write-Host $app
     }
-    $cmklabel = Get-CmkLabel -ComputerName $cmk
+    $cmklabel = Get-Cmkhostlabel -ComputerName $cmk -Connection $connection
     Write-Host $cmklabel
 }
